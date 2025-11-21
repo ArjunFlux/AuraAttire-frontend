5035c691c91a7ce50b4fcb33691f54565e018c23
Parth Upadhyay <upadhyayparth3010@gmail.com>
2025-11-21 13:39:18 +0530

    Update CheckAuthentication.js

--- BackEndDevelopment/Middleware/CheckAuthentication.js
+++ BackEndDevelopment/Middleware/CheckAuthentication.js
@@ -1,14 +1,1 @@
-import { useEffect } from 'react';
-import {useNavigate} from 'react-router-dom'
-export default function CheckAutehnication({children}){
-    const navigate = useNavigate();
-    useEffect(()=>{
-        const token = localStorage.getItem('token');
-        if(!token){
-            navigate('/login')
-        }
-    },[navigate])
-    return(
-        children
-    )
-}
+
